05c25eb3e45162da6783d7e2ffae84ee85445c07
Kimjunseon <junseon0722@gmail.com>
2023-01-02 17:56:54 +0900

    230102 Django

--- PychamProject/BulletinProject/bulletin_board/views.py
+++ PychamProject/BulletinProject/bulletin_board/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import  Bulletin
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils import timezone
 
 # Create your views here.
+def index(request):
+    bulletin_list = Bulletin.objects.all().order_by('-writeDate') # - desc 안 붙이면 asc
+    context = {'bulletin_list' : bulletin_list}
+    return render(request, 'bulletin/index.html', context)
+
+def create_bulletin(request):
+    return render(request, 'bulletin/create_bulletin.html')
+
+def add_bulletin(request):
+    bulletin = Bulletin()
+    bulletin.title = request.POST['title']
+    bulletin.content = request.POST['content']
+    bulletin.name = request.POST['name']
+    bulletin.passwd = request.POST['pincode']
+    bulletin.save()
+
+    return HttpResponseRedirect(reverse('bulletin_board:index'))
+
+def view_bulletin(request, bulletin_id):
+    # get_object_or_404: id 값이 없을 시, 404 에러 페이지를 띄운다.
+    bulletin = get_object_or_404(Bulletin, pk=bulletin_id) 
+    return render(request, 'bulletin/detail.html', {'bulletin' : bulletin})
+
+def update_bulletin(request, bulletin_id):
+    bulletin = Bulletin.objects.get(id=bulletin_id)
+
+    if request.method == 'POST':
+        bulletin.title = request.POST['title']
+        bulletin.content = request.POST['content']
+        bulletin.writeDate = timezone.datetime.now()
+        bulletin.save()
+        return HttpResponseRedirect(reverse('bulletin_board:view', args=(bulletin_id,)))
+    else:
+        return render(request, 'bulletin/detail.html', {'bulletin' : bulletin})
+
+
+def delete_bulletin(request, bulletin_id):
+    bulletin = Bulletin.objects.get(id=bulletin_id)
+    bulletin.delete()
+    return HttpResponseRedirect(reverse('bulletin_board:index'))
+
